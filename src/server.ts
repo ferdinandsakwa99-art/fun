5b2c77653d8cddc121f0131779798f863873d13f
Ochibo <bochieng442@gmail.com>
2026-08-11 10:58:12 +0300

    changed port

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app, { httpServer } from './app';
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
-httpServer.listen(PORT, () => {
+httpServer.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
